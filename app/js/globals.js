cbd8c8611efc6f8db654341f53fd0c56a8edce9d
Doug Sherk <github@sherk.me>
2015-01-15 13:48:40 -0500

    Import gaia-component properly

--- app/js/globals.js
+++ app/js/globals.js
@@ -1,7 +1,10 @@
+/* globals requirejs */
+
 window.COMPONENTS_BASE_URL = './components/';
 
 requirejs.config({
   paths: {
+    'gaia-component': '../components/gaia-component/gaia-component',
     'gaia-icons': '../components/gaia-icons/gaia-icons'
   }
 });
